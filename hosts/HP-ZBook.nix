4a5aba3a5dacc005dc8f320f850bf98ca1104a6e
Collin Diekvoss <Collin@Diekvoss.com>
2023-03-27 13:51:39 -0500

    add HP-ZBook config

--- hosts/HP-ZBook.nix
+++ hosts/HP-ZBook.nix
@@ -4,7 +4,24 @@
 in nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
+    ../system/filesystem/btrfs.nix
+    ../system/filesystem/efi.nix
     ../system/gnome.nix
+    ({ lib, ... }: {
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.efi.efiSysMountPoint = "/boot/efi";
+      networking.hostName = "HP-ZBook";
+      boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ "kvm-intel" ];
+      boot.extraModulePackages = [ ];
+      swapDevices = [ ];
+      networking.useDHCP = lib.mkDefault true;
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+      hardware.cpu.intel.updateMicrocode = true;
+    })
     nixpkgs.nixosModules.notDetected
     home-manager.nixosModules.home-manager {
       home-manager.useGlobalPkgs = true;
