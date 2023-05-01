b1bb72e2e309584a5d6d66df3405da6760668e71
Collin Diekvoss <Collin@Diekvoss.com>
2023-05-01 09:01:49 -0500

    use kea in place of dnsmasq

--- hosts/router.nix
+++ hosts/router.nix
@@ -10,8 +10,15 @@ in nixpkgs.lib.nixosSystem {
     ../system/nixos.nix
     ({ lib, ... }: {
       boot = {
-        initrd.availableKernelModules =
-          [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
+        initrd.availableKernelModules = [
+          "xhci_pci"
+          "ahci"
+          "nvme"
+          "usb_storage"
+          "usbhid"
+          "sd_mod"
+          "sdhci_pci"
+        ];
         initrd.kernelModules = [ ];
         kernelModules = [ "kvm-intel" ];
         extraModulePackages = [ ];
@@ -78,37 +85,91 @@ in nixpkgs.lib.nixosSystem {
           interfaces.br0.allowedUDPPorts = [ 53 ];
         };
       };
-      services.openssh.openFirewall = false;
-      services.dnsmasq = {
-        enable = true;
-        settings = {
-          server = [ "10.1.0.6" "1.1.1.1" "1.0.0.1" ];
-          domain-needed = true;
-          interface = [ "br0" "cdwifi" "cdiot" "cdguest" ];
-          dhcp-range = [
-            "10.1.0.100,10.1.0.199"
-            "10.1.10.100,10.1.10.199"
-            "10.1.20.100,10.1.20.199"
-            "10.1.30.2,10.1.30.254"
-          ];
-          dhcp-host = [
-            # Omada Controller
-            "10:27:f5:bd:04:97,10.1.0.2"
-            # Proxmox
-            "70:85:c2:8a:53:5b,10.1.10.3"
-            # TrueNAS VM (Proxmox)
-            "e2:8b:29:5e:56:ca,10.1.10.4"
-            # Canon Printer
-            "c4:ac:59:a6:63:33,10.1.10.5"
-            # Docker VM (Proxmox)
-            "7a:8d:bd:a3:66:ba,10.1.10.6"
-          ];
+      services = {
+        openssh.openFirewall = false;
+        kea.dhcp4 = {
+          enable = true;
+          settings = {
+            interfaces-config = {
+              interfaces = [ "br0" "cdwifi" "cdiot" "cdguest" ];
+            };
+            rebind-timer = 2000;
+            renew-timer = 1000;
+            subnet4 = [
+              {
+                pools = [{ pool = "10.1.0.100 - 10.1.0.240"; }];
+                subnet = "10.1.0.0/24";
+                interface = "br0";
+                reservations-in-subnet = true;
+                reservations = [{
+                  hw-address = "10:27:f5:bd:04:97";
+                  ip-address = "10.1.0.2";
+                  hostname = "omada";
+                }];
+                option-data = [{
+                  name = "domain-name-servers";
+                  data = "1.1.1.1, 1.0.0.1";
+                }];
+              }
+              {
+                pools = [{ pool = "10.1.10.100 - 10.1.10.240"; }];
+                subnet = "10.1.10.0/24";
+                interface = "cdwifi";
+                reservations-in-subnet = true;
+                reservations = [
+                  {
+                    hw-address = "70:85:c2:8a:53:5b";
+                    ip-address = "10.1.10.3";
+                    hostname = "proxmox";
+                  }
+                  {
+                    hw-address = "e2:8b:29:5e:56:ca";
+                    ip-address = "10.1.10.4";
+                    hostname = "truenas";
+                  }
+                  {
+                    hw-address = "c4:ac:59:a6:63:33";
+                    ip-address = "10.1.10.5";
+                    hostname = "canon";
+                  }
+                  {
+                    hw-address = "7a:8d:bd:a3:66:ba";
+                    ip-address = "10.1.10.6";
+                    hostname = "docker";
+                  }
+                ];
+                option-data = [{
+                  name = "domain-name-servers";
+                  data = "10.1.0.6, 1.1.1.1, 1.0.0.1";
+                }];
+              }
+              {
+                pools = [{ pool = "10.1.20.100 - 10.1.20.240"; }];
+                subnet = "10.1.20.0/24";
+                interface = "cdiot";
+                option-data = [{
+                  name = "domain-name-servers";
+                  data = "10.1.0.6, 1.1.1.1, 1.0.0.1";
+                }];
+              }
+              {
+                pools = [{ pool = "10.1.30.100 - 10.1.30.240"; }];
+                subnet = "10.1.30.0/24";
+                interface = "cdguest";
+                option-data = [{
+                  name = "domain-name-servers";
+                  data = "1.1.1.1, 1.0.0.1";
+                }];
+              }
+            ];
+            valid-lifetime = 4000;
+          };
         };
-      };
-      services.avahi = {
-        enable = true;
-        reflector = true;
-        allowInterfaces = [ "cdwifi" "cdiot" ];
+        avahi = {
+          enable = true;
+          reflector = true;
+          allowInterfaces = [ "cdwifi" "cdiot" ];
+        };
       };
     })
     nixpkgs.nixosModules.notDetected
