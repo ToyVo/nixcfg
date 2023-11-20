344ddcc601af78f0f9795f9b060bb5fa3f65940e
Collin Diekvoss <Collin@Diekvoss.com>
2023-11-19 21:00:46 -0600

    pi

---
+++ systems/aarch64-linux/rpi4b8b/default.nix
@@ -0,0 +1,14 @@
+{ lib, ... }: {
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  networking.hostName = "rpi4b8b";
+  boot = {
+    loader.grub.enable = false;
+    loader.generic-extlinux-compatible.enable = true;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+  };
+  cd = {
+    defaults.enable = true;
+    users.toyvo.enable = true;
+    fs.sd.enable = true;
+  };
+}
