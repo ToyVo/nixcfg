0d36624479fd9e533337d426022bd2a915f8c8ca
Collin Diekvoss <Collin@Diekvoss.com>
2023-11-26 17:37:43 -0600

    utm

---
+++ modules/nixos/filesystems/ext4/default.nix
@@ -0,0 +1,26 @@
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.cd;
+in
+{
+  options.cd.fs.sd.enable = lib.mkEnableOption "root partition";
+  options.cd.fs.ext4.enable = lib.mkEnableOption "root ext4 partition";
+  options.cd.fs.ext4.label = lib.mkOption {
+    type = types.string;
+    default = "NIXOS";
+    internal = true;
+  };
+
+  config = {
+
+    cd.fs.ext4 = lib.mkIf cfg.fs.sd.enable {
+      enable = true;
+      label = "NIXOS_SD";
+    };
+
+    fileSystems."/" = lib.mkIf cfg.fs.ext4.enable {
+      device = "/dev/disk/by-label/${cfg.fs.ext4.label}";
+      fsType = "ext4";
+    };
+  };
+}
---
+++ systems/aarch64-linux/utm/default.nix
@@ -0,0 +1,16 @@
+{ lib, ... }: {
+  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+  networking.hostName = "utm";
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "usbhid" "usb_storage" "sr_mod" ];
+  };
+  cd = {
+    defaults.enable = true;
+    users.toyvo.enable = true;
+    fs.boot.enable = true;
+    fs.ext4.enable = true;
+    desktops.cosmic.enable = true;
+  };
+}
