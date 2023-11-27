88063826772b30dac2927b5ff6c81d514ea9c9a7
Collin Diekvoss <Collin@Diekvoss.com>
2023-11-26 18:31:12 -0600

    cosmic

--- systems/aarch64-linux/utm/default.nix
+++ systems/aarch64-linux/utm/default.nix
@@ -1,5 +1,5 @@
-{ lib, ... }: {
-  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+{ lib, modulesPath ... }: {
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   networking.hostName = "utm";
   boot = {
     loader.systemd-boot.enable = true;
