9effa522f15f1a283f73b84c0eca9063d9ca3458
Collin Diekvoss <Collin@Diekvoss.com>
2023-11-29 09:56:16 -0600

    homer

--- modules/nixos/homer/default.nix
+++ modules/nixos/homer/default.nix
@@ -14,7 +14,7 @@ in
       oci-containers.containers = {
         homer = {
           image = "b4bz/homer:latest";
-          ports = [ "0.0.0.0:8080:8080" ];
+          ports = [ "127.0.0.1:8080:8080" ];
           volumes = [ "/etc/homer:/www/assets" ];
         };
       };
