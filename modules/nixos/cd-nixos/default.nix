d90f3cb48849cb7cf7ee0a08f2871f5263f22f8d
Collin Diekvoss <Collin@Diekvoss.com>
2023-12-12 21:11:07 -0600

    appimage

--- modules/nixos/cd-nixos/default.nix
+++ modules/nixos/cd-nixos/default.nix
@@ -78,5 +78,13 @@ in
       pulse.enable = cfg.packages.gui.enable;
     };
     fonts.packages = with pkgs; lib.mkIf cfg.packages.gui.enable [ monaspace (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; }) ];
+    boot.binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
   };
 }
