8be9ed773cc272ad1d12b492ad85458f23b1c9ee
Collin Diekvoss <Collin@Diekvoss.com>
2023-12-21 16:17:42 -0600

    deck

--- homes/x86_64-linux/deck@steamdeck/default.nix
+++ homes/x86_64-linux/deck@steamdeck/default.nix
@@ -1,11 +1,40 @@
 { pkgs, ... }:
 {
-    # Your configuration.
-    home.username = "deck";
-    home.homeDirectory = "/home/deck";
-    home.stateVersion = "23.11";
-    programs.home-manager.enable = true;
-    home.packages = with pkgs; [
-      obs-studio-plugins.obs-vkcapture
-    ];
+  home.username = "deck";
+  home.homeDirectory = "/home/deck";
+  home.stateVersion = "23.11";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  home.sessionPath = [ "$HOME/.local/bin" ];
+  home.packages = with pkgs; [
+    obs-studio-plugins.obs-vkcapture
+  ];
+  xdg.configFile."nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+  '';
+  xdg.configFile."nixpkgs/config.nix".text = ''
+    { allowUnfree = true; }
+  '';
+  programs.home-manager.enable = true;
+  programs.starship = {
+    enable = true;
+    settings = {
+      right_format = "$time";
+      time.disabled = false;
+    };
+  };
+  programs.zoxide.enable = true;
+  cd.packages = {
+    bat.enable = true;
+    eza.enable = true;
+    git.enable = true;
+    gpg.enable = true;
+    helix.enable = true;
+    ssh.enable = true;
+    zsh.enable = true;
+    bash.enable = true;
+    fish.enable = true;
+    nushell.enable = true;
+  };
 }
