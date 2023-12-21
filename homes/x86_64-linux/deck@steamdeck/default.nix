105bd54ded4ecd95e7aabea3f8938e34c91b09a6
Collin Diekvoss <Collin@Diekvoss.com>
2023-12-21 15:58:47 -0600

    deck

--- homes/x86_64-linux/deck@steamdeck/default.nix
+++ homes/x86_64-linux/deck@steamdeck/default.nix
@@ -1,23 +1,11 @@
-{
-    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-    # as well as the libraries available from your flake's inputs.
-    lib,
-    # An instance of `pkgs` with your overlays and packages applied is also available.
-    pkgs,
-    # You also have access to your flake's inputs.
-    inputs,
-
-    # Additional metadata is provided by Snowfall Lib.
-    home, # The home architecture for this host (eg. `x86_64-linux`).
-    target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
-    format, # A normalized name for the home target (eg. `home`).
-    virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
-    host, # The host name for this home.
-
-    # All other arguments come from the home home.
-    config,
-    ...
-}:
+{ pkgs, ... }:
 {
     # Your configuration.
+    home.username = "deck";
+    home.homeDirectory = "/home/deck";
+    home.stateVersion = "23.11";
+    programs.home-manager.enable = true;
+    home.packages = with pkgs; [
+      obs-studio-plugins.obs-vkcapture
+    ];
 }
