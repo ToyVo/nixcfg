135aed3e2e9a93f3d8256a3a0f255ec226aca81b
Collin Diekvoss <Collin@Diekvoss.com>
2023-12-21 16:20:29 -0600

    deck

--- homes/x86_64-linux/deck@steamdeck/default.nix
+++ homes/x86_64-linux/deck@steamdeck/default.nix
@@ -25,16 +25,25 @@
     };
   };
   programs.zoxide.enable = true;
-  cd.packages = {
-    bat.enable = true;
-    eza.enable = true;
-    git.enable = true;
-    gpg.enable = true;
-    helix.enable = true;
-    ssh.enable = true;
-    zsh.enable = true;
-    bash.enable = true;
-    fish.enable = true;
-    nushell.enable = true;
-  };
+  imports = [
+    {
+      options.cd.username = lib.mkOption {
+        type = lib.types.str;
+        default = "deck";
+      };
+      config.cd.packages = {
+        bat.enable = true;
+        eza.enable = true;
+        git.enable = true;
+        gpg.enable = true;
+        helix.enable = true;
+        ssh.enable = true;
+        zellij.enable = true;
+        zsh.enable = true;
+        bash.enable = true;
+        fish.enable = true;
+        nushell.enable = true;
+      };
+    }
+  ];
 }
