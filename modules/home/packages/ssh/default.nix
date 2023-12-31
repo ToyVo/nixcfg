da055a43703ad1c9457144e24b71a005c650a735
Collin Diekvoss <Collin@Diekvoss.com>
2023-12-30 22:52:00 -0600

    start ssh-agent

--- modules/home/packages/ssh/default.nix
+++ modules/home/packages/ssh/default.nix
@@ -6,24 +6,35 @@ in
   options.cd.packages.ssh.enable = lib.mkEnableOption "Enable ssh";
 
   config = lib.mkIf cfg.packages.ssh.enable {
-    programs.ssh = {
-      enable = true;
-      matchBlocks."10.1.0.*" = {
-        identitiesOnly = true;
-        identityFile = [
-          "~/.ssh/ykC_ed25519_sk"
-          "~/.ssh/ykA_ed25519_sk"
-        ];
-        extraOptions.AddKeysToAgent = "yes";
+    programs = {
+      ssh = {
+        enable = true;
+        matchBlocks."10.1.0.*" = {
+          identitiesOnly = true;
+          identityFile = [
+            "~/.ssh/ykC_ed25519_sk"
+            "~/.ssh/ykA_ed25519_sk"
+          ];
+          extraOptions.AddKeysToAgent = "yes";
+        };
+        matchBlocks."github.com" = {
+          identitiesOnly = true;
+          identityFile = [
+            "~/.ssh/ykC_ed25519_sk"
+            "~/.ssh/ykA_ed25519_sk"
+          ];
+          extraOptions.AddKeysToAgent = "yes";
+        };
       };
-      matchBlocks."github.com" = {
-        identitiesOnly = true;
-        identityFile = [
-          "~/.ssh/ykC_ed25519_sk"
-          "~/.ssh/ykA_ed25519_sk"
-        ];
-        extraOptions.AddKeysToAgent = "yes";
-      };
+      zsh.initExtra = ''
+        eval $(ssh-agent -s)
+      '';
+      bash.initExtra = ''
+        eval $(ssh-agent -s)
+      '';
+      fish.interactiveShellInit = ''
+        eval $(ssh-agent -c)
+      '';
     };
   };
 }
