b73f773e13d5e2cbbb249e65fe88c943aead0a31
Collin Diekvoss <Collin@Diekvoss.com>
2024-02-01 08:41:29 -0600

    nextcloud

--- modules/nixos/containers/nextcloud.nix
+++ modules/nixos/containers/nextcloud.nix
@@ -8,9 +8,14 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    virtualisation.oci-containers.containers.nextcloud = {
+    virtualisation.oci-containers.containers.nextcloud-aio-mastercontainer = {
       image = "nextcloud/all-in-one:latest";
       autoStart = true;
+      ports = [ "80:80" "443:443" "8443:8443" ];
+      volumes = [
+        "/mnt/POOL/nextcloud-aio-mastercontainer:/mnt/docker-aio-coinfig"
+        "/run/docker.sock:/var/run/docker.sock:ro"
+      ];
     };
   };
 }
