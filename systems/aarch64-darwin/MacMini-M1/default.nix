c0557aedb7a90c92a0ed2144c28e0efa9f7a330b
Collin Diekvoss <Collin@Diekvoss.com>
2024-02-09 19:43:07 -0600

    macmini

--- systems/aarch64-darwin/MacMini-M1/default.nix
+++ systems/aarch64-darwin/MacMini-M1/default.nix
@@ -6,15 +6,18 @@
     { name = "google-chrome"; greedy = true; }
   ];
   environment.systemPackages = with pkgs; let
-    pymobiledevice3 = (buildPythonPackage {
+    pymobiledevice3 = python3Packages.buildPythonPackage {
       pname = "pymobiledevice3 ";
       version = "2.30.0";
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "";
-      });
-  in; [
-    (python3.withPackages ([
+      format = "wheel";
+      src = fetchurl {
+        url = "https://files.pythonhosted.org/packages/7c/f2/070be3672904106664d5048d52a709b4ef0204772179c202419c57365c59/pymobiledevice3-2.30.0-py3-none-any.whl";
+        sha256 = "sha256-X9Gs7HM5N9KdyndO3+sROxyfp8PcCdN5N48cwLd0HR4=";
+      };
+    };
+  in [
+    openssl
+    (python3.withPackages (ps: [
       pymobiledevice3
     ]))
   ];
