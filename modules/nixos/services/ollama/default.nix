f0de21e9d91ac64d64d18711e50c1b7d2b1cb934
Collin Diekvoss <Collin@Diekvoss.com>
2024-02-21 13:59:45 -0600

    ollama

--- modules/nixos/services/ollama/default.nix
+++ modules/nixos/services/ollama/default.nix
@@ -3,17 +3,24 @@ let
   cfg = config.services.ollama;
 in
 {
-  options.services.ollama.openFirewall = lib.mkOption {
-    type = lib.types.bool;
-    default = true;
-    description = "Whether to open the firewall for the Ollama service.";
+  options.services.ollama = {
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to open the firewall for the Ollama service.";
+    };
+    port = lib.mkOption {
+      type = lib.types.int;
+      default = 11434;
+      description = "The port on which the Ollama service listens.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    services.ollama.listenAddress = "0.0.0.0:${toString cfg.port}";
     networking = lib.mkIf cfg.openFirewall {
       firewall = {
-        allowedTCPPorts = [ 11434 ];
-        allowedUDPPorts = [ 11434 ];
+        allowedTCPPorts = [ cfg.port ];
       };
     };
   };
