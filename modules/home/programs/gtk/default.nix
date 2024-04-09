e6a641540f22b3b2025b00023cd44196b842907e
Collin Diekvoss <Collin@Diekvoss.com>
2024-04-08 22:02:24 -0500

    gtk

--- modules/home/programs/gtk/default.nix
+++ modules/home/programs/gtk/default.nix
@@ -1,15 +1,15 @@
 { pkgs, lib, config, ... }:
 let 
   cfg = config.profiles;
-  gtk_2_3_4_attrs = {
-    gtk-enable-animations = true;
-    gtk-primary-button-warps-slider = true;
-    gtk-sound-theme-name = "ocean";
-  };
   gtk_2_3_attrs = {
     gtk-button-images = true;
     gtk-menu-images = true;
     gtk-toolbar-style = 3;
+  };
+  gtk_2_3_4_attrs = {
+    gtk-enable-animations = true;
+    gtk-primary-button-warps-slider = true;
+    gtk-sound-theme-name = "ocean";
   };
   gtk_3_4_attrs = {
     gtk-application-prefer-dark-theme = true;
@@ -17,8 +17,6 @@ let
     gtk-modules = "colorreload-gtk-module";
     gtk-xft-dpi = 98304;
   };
-  toGtk2 = value: if lib.isString value then "\"${value}\"" else toString value;
-  toGtk = value: if lib.isBool value then if value then "true" else "false" else toString value;
 in 
 {
   config = lib.mkIf (cfg.defaults.enable && cfg.gui.enable && pkgs.stdenv.isLinux) {
@@ -30,18 +28,9 @@ in
       };
       iconTheme.name = "Papirus-Dark";
       cursorTheme.size = 24;
-      gtk2.extraConfig = ''
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk2 value}") gtk_2_3_attrs)}
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk2 value}") gtk_2_3_4_attrs)}
-      '';
-      gtk3.extraConfig = ''
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk value}") gtk_2_3_attrs)}
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk value}") gtk_3_4_attrs)}
-      '';
-      gtk4.extraConfig = ''
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk value}") gtk_2_3_attrs)}
-        ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${toGtk value}") gtk_3_4_attrs)}
-      '';
+      gtk2.extraConfig = lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name}=${if lib.isString value then "\"${value}\"" else toString value}") (gtk_2_3_attrs // gtk_2_3_4_attrs));
+      gtk3.extraConfig = gtk_2_3_attrs // gtk_2_3_4_attrs // gtk_3_4_attrs;
+      gtk4.extraConfig = gtk_2_3_4_attrs // gtk_3_4_attrs;
       catppuccin = {
         enable = true;
         flavour = config.catppuccin.flavour;
