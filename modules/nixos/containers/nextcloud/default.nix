2bf15b39c2426672a15479bac7c43fcb0d58f628
Collin Diekvoss <Collin@Diekvoss.com>
2024-04-22 19:55:23 -0500

    nextcloud

--- modules/nixos/containers/nextcloud/default.nix
+++ modules/nixos/containers/nextcloud/default.nix
@@ -14,8 +14,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    containerPresets.docker.enable = lib.mkDefault true;
-    virtualisation.oci-containers.containers.nextcloud = {
+    containerPresets.podman.enable = lib.mkDefault true;
+    virtualisation.oci-containers.containers."nextcloud-aio-mastercontainer" = {
       image = "docker.io/nextcloud/all-in-one:latest";
       volumes = [
         "nextcloud_aio_mastercontainer:/mnt/docker-aio-config"
