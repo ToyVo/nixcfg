d89c0ac54c126acdda194f91dddbffdf210a9d58
Collin Diekvoss <Collin@Diekvoss.com>
2024-06-11 11:12:32 -0500

    disksize

--- systems/oracle.nix
+++ systems/oracle.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, config, pkgs, nixos-unstable, ... }: {
   networking.hostName = "oracle-ampere-nixos";
   boot = {
     initrd = {
@@ -8,4 +8,12 @@
   };
   profiles.defaults.enable = true;
   userPresets.toyvo.enable = true;
+  system.build.OCIImage = lib.mkForce (import "${nixos-unstable}/lib/make-disk-image.nix" {
+    inherit config lib pkgs;
+    name = "oci-image";
+    configFile = "${nixos-unstable}/nixos/modules/virtualisation/make-disk-image.nix";
+    format = "qcow2";
+    diskSize = 16384;
+    partitionTableType = if config.oci.efi then "efi" else "legacy";
+  });
 }
