fda5da17232fef4e06d0ad13d9f6c8d073bcf966
Collin Diekvoss <Collin@Diekvoss.com>
2024-06-11 09:00:18 -0500

    oracle

--- systems/oracle.nix
+++ systems/oracle.nix
@@ -1,9 +1,10 @@
-{ ... }: {
+{ lib, ... }: {
   networking.hostName = "oracle-ampere-nixos";
   boot = {
     initrd = {
       availableKernelModules = [ "usbhid" ];
     };
+    loader.grub.splashImage = lib.mkForce null;
   };
   profiles.defaults.enable = true;
   userPresets.toyvo.enable = true;
