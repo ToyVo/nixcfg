c947c9fb63c843b117e9ae900ea24df44ed263c3
Collin Diekvoss <Collin@Diekvoss.com>
2024-06-12 13:46:42 -0500

    ollamafirewall

--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -3,16 +3,11 @@ let
   cfg = config.services.ollama;
 in
 {
-  options.services.ollama = {
-    openFirewall = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Whether to open the firewall for the Ollama service.";
-    };
-  };
-
   config = lib.mkIf (cfg.enable && cfg.openFirewall) {
-    services.ollama.host = "0.0.0.0";
+    services.ollama = {
+      host = "0.0.0.0";
+      openFirewall = true;
+    };
     networking = {
       firewall = {
         allowedTCPPorts = [ cfg.port ];
