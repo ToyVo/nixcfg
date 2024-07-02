33ce96a632de1d8056dfe686b4ee25ee723c58e8
Collin Diekvoss <Collin@Diekvoss.com>
2024-07-02 12:54:18 -0500

    fix

--- systems/FQ-M-4CP7WX04/home.nix
+++ systems/FQ-M-4CP7WX04/home.nix
@@ -2,25 +2,27 @@
 {
   imports = [ ../../secrets/secrets.nix ];
 
-  programs = {
-    ssh.matchBlocks."github-emu" = {
-      hostname = "github.com";
-      identitiesOnly = true;
-      identityFile = config.sops.secrets.emu_ed25519.path;
-    };
-    gpg.publicKeys = [{
-      source = ../../secrets/gpg_emu.pub;
-      trust = 5;
-    }];
+  config = {
     sops.secrets = {
       ykW_ed25519_sk.mode = "0600";
       "ykW_ed25519_sk.pub".mode = "0644";
       emu_ed25519.mode = "0600";
       "emu_ed25519.pub".mode = "0644";
     };
-    volta = {
-      enable = true;
-      package = pkgs.emptyDirectory;
+    programs = {
+      ssh.matchBlocks."github-emu" = {
+        hostname = "github.com";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets.emu_ed25519.path;
+      };
+      gpg.publicKeys = [{
+        source = ../../secrets/gpg_emu.pub;
+        trust = 5;
+      }];
+      volta = {
+        enable = true;
+        package = pkgs.emptyDirectory;
+      };
     };
   };
 }
