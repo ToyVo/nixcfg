340747356ab20921bcdde94aeea2bac8e2c9f9e9
Collin Diekvoss <Collin@Diekvoss.com>
2024-08-07 18:46:09 -0500

    ssh

--- systems/FQ-M-4CP7WX04/home.nix
+++ systems/FQ-M-4CP7WX04/home.nix
@@ -8,12 +8,23 @@
       "ykW_ed25519_sk.pub".mode = "0644";
       emu_ed25519.mode = "0600";
       "emu_ed25519.pub".mode = "0644";
+      id_ed25519.mode = "0600";
+      "id_ed25519.pub".mode = "0644";
     };
     programs = {
-      ssh.matchBlocks."github-emu" = {
-        hostname = "github.com";
-        identitiesOnly = true;
-        identityFile = config.sops.secrets.emu_ed25519.path;
+      ssh.matchBlocks = {
+        "github-emu" = {
+          hostname = "github.com";
+          identitiesOnly = true;
+          identityFile = config.sops.secrets.emu_ed25519.path;
+        };
+        "github.com" = {
+          identityFile = lib.mkForce [
+            config.sops.secrets.id_ed25519.path
+            config.sops.secrets.ykC_ed25519_sk.path
+            config.sops.secrets.ykA_ed25519_sk.path
+          ];
+        };
       };
       gpg.publicKeys = [{
         source = ../../secrets/gpg_emu.pub;
