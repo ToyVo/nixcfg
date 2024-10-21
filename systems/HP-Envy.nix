9b0dddbc6b70eab372d227faa8458573f69159f1
Collin Diekvoss <Collin@Diekvoss.com>
2024-10-20 21:38:28 -0500

    envy

--- systems/HP-Envy.nix
+++ systems/HP-Envy.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   hardware.cpu.amd.updateMicrocode = true;
   networking.hostName = "HP-Envy";
@@ -23,9 +23,12 @@
     gui.enable = true;
   };
   userPresets.toyvo.enable = true;
+  userPresets.chloe.enable = true;
   fileSystemPresets.boot.enable = true;
   fileSystemPresets.btrfs.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.cosmic-greeter.enable = lib.mkForce false;
   services.xserver.videoDrivers = [ "amdgpu" ];
   hardware.graphics = {
     extraPackages = with pkgs; [
@@ -37,4 +40,22 @@
     enable = true;
     enable32Bit = true;
   };
+  environment.systemPackages = with pkgs; [
+    discord
+    heroic
+    lutris
+    mangohud
+    prismlauncher
+    protonup
+    r2modman
+    steam
+    steamPackages.steamcmd
+    (wrapOBS {
+      plugins = with obs-studio-plugins; [
+        obs-gstreamer
+        obs-vaapi
+        obs-vkcapture
+      ];
+    })
+  ];
 }
