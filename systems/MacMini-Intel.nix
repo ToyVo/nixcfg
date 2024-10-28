0c0d2809dd8da977cc1b93d9aabb8e579c6a617d
Collin Diekvoss <Collin@Diekvoss.com>
2024-10-28 17:31:22 -0500

    token

--- systems/MacMini-Intel.nix
+++ systems/MacMini-Intel.nix
@@ -10,11 +10,8 @@
     nh_darwin = {
       enable = true;
       name = config.networking.hostName;
-      tokenFile = config.sops.secrets.gha_nh_darwin.path;
+      tokenFile = "/secrets/gha_nh_darwin";
       url = "https://github.com/toyvo/nh_darwin";
     };
-  };
-  sops.secrets = {
-    gha_nh_darwin = { };
   };
 }
