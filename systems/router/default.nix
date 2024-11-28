fae5ac4f0791d35a8e69cb6be36d58a49d06df41
Collin Diekvoss <Collin@Diekvoss.com>
2024-11-27 18:46:43 -0600

    packwin

--- systems/router/default.nix
+++ systems/router/default.nix
@@ -73,36 +73,115 @@
   userPresets.toyvo.enable = true;
   fileSystemPresets.boot.enable = true;
   fileSystemPresets.btrfs.enable = true;
-  systemd.network = {
-    enable = true;
-    networks.wan0 = {
-      matchConfig.Name = "enp2s0";
-      networkConfig.DHCP = "ipv4";
-      dhcpV4Config = {
-        UseDNS = false;
+  systemd = {
+    network = {
+      enable = true;
+      networks.wan0 = {
+        matchConfig.Name = "enp2s0";
+        networkConfig.DHCP = "ipv4";
+        dhcpV4Config = {
+          UseDNS = false;
+        };
+        linkConfig.RequiredForOnline = "routable";
       };
-      linkConfig.RequiredForOnline = "routable";
-    };
-    networks.lan0 = {
-      matchConfig.Name = "enp3s0 enp4s0 enp5s0";
-      networkConfig.Bridge = "br0";
-    };
-    networks.br0 = {
-      matchConfig.Name = "br0";
-      networkConfig = {
-        Address = "10.1.0.1/24";
-        IPMasquerade = "ipv4";
-        MulticastDNS = true;
+      networks.lan0 = {
+        matchConfig.Name = "enp3s0 enp4s0 enp5s0";
+        networkConfig.Bridge = "br0";
+      };
+      networks.br0 = {
+        matchConfig.Name = "br0";
+        networkConfig = {
+          Address = "10.1.0.1/24";
+          IPMasquerade = "ipv4";
+          MulticastDNS = true;
+        };
+      };
+      netdevs.br0.netdevConfig = {
+        Name = "br0";
+        Kind = "bridge";
+        MACAddress = "none";
+      };
+      links.br0 = {
+        matchConfig.OriginalName = "br0";
+        linkConfig.MACAddressPolicy = "none";
       };
     };
-    netdevs.br0.netdevConfig = {
-      Name = "br0";
-      Kind = "bridge";
-      MACAddress = "none";
-    };
-    links.br0 = {
-      matchConfig.OriginalName = "br0";
-      linkConfig.MACAddressPolicy = "none";
+    services = {
+      cfdyndns = {
+        serviceConfig.Type = "oneshot";
+        after = [ "network.target" ];
+        path = with pkgs; [
+          curl
+          iproute2
+          gawk
+          dig
+          jq
+        ];
+        script = ''
+          declare -a DOMAINS=(
+            "*.diekvoss.net"
+            "packwiz.toyvo.dev"
+          )
+          TOKEN=$(cat ${config.sops.secrets.cloudflare_w_dns_r_zone_token.path})
+
+          function put_record() {
+            curl -sS -X PUT \
+              -H "Content-Type: application/json" \
+              -H "Authorization: Bearer $TOKEN" \
+              -d "{\"type\":\"A\",\"name\":\"$3\",\"content\":\"$4\",\"ttl\":1,\"proxied\":false}" \
+              "https://api.cloudflare.com/client/v4/zones/$1/dns_records/$2"
+          }
+
+          function get_ip() {
+            curl -sS \
+               -H "Content-Type: application/json" \
+               -H "Authorization: Bearer $TOKEN" \
+               https://api.cloudflare.com/client/v4/zones/$1/dns_records/$2 | jq -r '.result.content'
+          }
+
+          function get_zone() {
+            curl -sS \
+               -H "Content-Type: application/json" \
+               -H "Authorization: Bearer $TOKEN" \
+               https://api.cloudflare.com/client/v4/zones?name=$1 | jq -r '.result.[].id'
+          }
+
+          function get_record() {
+            curl -sS \
+               -H "Content-Type: application/json" \
+               -H "Authorization: Bearer $TOKEN" \
+               https://api.cloudflare.com/client/v4/zones/$1/dns_records?name=$2 | jq -r '.result.[].id'
+          }
+
+          NEW_IP=$(ip addr show dev enp2s0 | awk '/inet / {print $2}' | cut -d '/' -f1)
+          echo "The IP Address of this machine is $NEW_IP"
+          for DOMAIN in "''${DOMAINS[@]}"
+          do
+              CURRENT_IP=$(dig +short $DOMAIN)
+              echo "DNS for $DOMAIN is currently set to $CURRENT_IP"
+              if [ "$CURRENT_IP" != "$NEW_IP" ]; then
+                echo "DNS for $DOMAIN Doesn't point to $NEW_IP, checking for confirmation..."
+                BASE_DOMAIN=$(awk -F'.' '{gsub(/^\*\./, ""); print $(NF-1) "." $NF}' <<< "$DOMAIN")
+                echo "Base for $DOMAIN is $BASE_DOMAIN"
+                ZONE=$(get_zone "$BASE_DOMAIN")
+                echo "Zone ID for $BASE_DOMAIN is $ZONE"
+                RECORD=$(get_record "$ZONE" "$DOMAIN")
+                echo "Record ID for $DOMAIN is $RECORD"
+                CONFIRM_IP=$(get_ip "$ZONE" "$RECORD")
+                echo "DNS for $DOMAIN is confirmed set to $CONFIRM_IP"
+                if [ "$CONFIRM_IP" != "$NEW_IP" ]; then
+                  echo "Updating DNS record for $DOMAIN to $NEW_IP"
+                  put_record "$ZONE" "$RECORD" "$DOMAIN" "$NEW_IP"
+                else
+                  echo "DNS record for $DOMAIN is already set to $NEW_IP, skipping update. Assuming TTL."
+                fi
+              else
+                echo "DNS record for $DOMAIN is $NEW_IP, skipping update."
+              fi
+          done
+        '';
+        startAt = "*:0/5";
+      };
     };
   };
   services = {
@@ -124,15 +203,6 @@
         bind_hosts = [ "127.0.1.53" ];
         bootstrap_dns = [ "9.9.9.9" ];
       };
-    };
-    cfdyndns = {
-      enable = true;
-      email = "collin@diekvoss.com";
-      apiTokenFile = config.sops.secrets.cloudflare_w_dns_r_zone_token.path;
-      records = [
-        "*.diekvoss.net"
-        "packwiz.toyvo.dev"
-      ];
     };
     caddy = {
       enable = true;
