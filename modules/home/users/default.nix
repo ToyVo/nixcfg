1b3f42741f9ddd49a11e4a9513d4448d73d97659
Collin Diekvoss <Collin@Diekvoss.com>
2024-12-23 21:46:27 -0600

    no longer using crate2nix

--- modules/home/users/default.nix
+++ modules/home/users/default.nix
@@ -66,8 +66,8 @@ in
     xdg.configFile = {
       "nix/nix.conf".text = ''
         experimental-features = nix-command flakes
-        substituters = https://cache.nixos.org https://nix-community.cachix.org https://cosmic.cachix.org https://toyvo.cachix.org https://eigenvalue.cachix.org
-        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE= toyvo.cachix.org-1:s++CG1te6YaS9mjICre0Ybbya2o/S9fZIyDNGiD4UXs= eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=
+        substituters = https://cache.nixos.org https://nix-community.cachix.org https://cosmic.cachix.org https://toyvo.cachix.org
+        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE= toyvo.cachix.org-1:s++CG1te6YaS9mjICre0Ybbya2o/S9fZIyDNGiD4UXs=
       '';
       "nixpkgs/config.nix".text = ''
         {
