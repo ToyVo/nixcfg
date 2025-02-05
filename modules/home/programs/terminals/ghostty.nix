36e560c678419bf21fb9c6bd98e4d8238339cc9f
Collin Diekvoss <Collin@Diekvoss.com>
2025-02-05 12:06:07 -0600

    headless ghostty

--- modules/home/programs/terminals/ghostty.nix
+++ modules/home/programs/terminals/ghostty.nix
@@ -2,7 +2,7 @@
 {
   home.packages =
     with pkgs;
-    lib.mkIf stdenv.isLinux && config.profiles.gui.enable [
+    lib.mkIf (stdenv.isLinux && config.profiles.gui.enable) [
       ghostty
     ];
   xdg.configFile."ghostty/config".text = ''
