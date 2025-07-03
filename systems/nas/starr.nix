c7ef7a71b7063d2b9e5e832030fe50507600e479
Collin Diekvoss <Collin@Diekvoss.com>
2025-07-03 14:05:09 -0500

    put transmission in ns

--- systems/nas/starr.nix
+++ systems/nas/starr.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 let
-wireguardInterface = "wg0";
-wireguardInterfaceNamespace = "protonvpn0";
+  wireguardInterface = "wg0";
+  wireguardInterfaceNamespace = "protonvpn0";
 in
 {
   sops.secrets."protonvpn-US-IL-503.key" = { };
@@ -93,22 +93,65 @@ in
       };
     };
     wireguard.interfaces.${wireguardInterface} = {
-        privateKeyFile = config.sops.secrets."protonvpn-US-IL-503.key".path;
-        ips = [ "10.2.0.2/32" ];
-        peers = [{
+      privateKeyFile = config.sops.secrets."protonvpn-US-IL-503.key".path;
+      ips = [ "10.2.0.2/32" ];
+      peers = [
+        {
           publicKey = "Ad0UnBi3NeIgVpM1baC8HAp6wfSli0wGS1OCmS7uYRo=";
           allowedIPs = [ "0.0.0.0/0" ];
           endpoint = "79.127.187.156:51820";
-        }];
-        interfaceNamespace = wireguardInterfaceNamespace;
-        preSetup = ''ip netns add "${wireguardInterfaceNamespace}"'';
-        postSetup = ''
-          ip -n "${wireguardInterfaceNamespace}" link set up dev "lo"
-          ip -n "${wireguardInterfaceNamespace}" route add default dev "${wireguardInterface}"
-        '';
-        preShutdown = ''ip -n "${wireguardInterfaceNamespace}" route del default dev "${wireguardInterface}"'';
-        postShutdown = ''ip netns del "${wireguardInterfaceNamespace}"'';
+        }
+      ];
+      interfaceNamespace = wireguardInterfaceNamespace;
+      preSetup = ''ip netns add "${wireguardInterfaceNamespace}"'';
+      postSetup = ''
+        ip -n "${wireguardInterfaceNamespace}" link set up dev "lo"
+        ip -n "${wireguardInterfaceNamespace}" route add default dev "${wireguardInterface}"
+      '';
+      preShutdown = ''ip -n "${wireguardInterfaceNamespace}" route del default dev "${wireguardInterface}"'';
+      postShutdown = ''ip netns del "${wireguardInterfaceNamespace}"'';
     };
   };
   environment.etc."netns/${wireguardInterfaceNamespace}/resolv.conf".text = "nameserver 10.2.0.1";
+  systemd = {
+    services = {
+      transmission = {
+        bindsTo = [ "wireguard-${wireguardInterface}.service" ];
+        requires = [
+          "network-online.target"
+          "wireguard-${wireguardInterface}.service"
+        ];
+        serviceConfig.NetworkNamespacePath = "/var/run/netns/${wireguardInterfaceNamespace}";
+      };
+      # creating proxy service on socket, which forwards the same port from the root namespace to the isolated namespace
+      proxy-to-transmission = {
+        enable = true;
+        description = "Proxy to Transmission in Network Namespace";
+        requires = [
+          "transmission.service"
+          "proxy-to-transmission.socket"
+        ];
+        after = [
+          "transmission.service"
+          "proxy-to-transmission.socket"
+        ];
+        unitConfig = {
+          JoinsNamespaceOf = "transmission.service";
+        };
+        serviceConfig = {
+          User = "transmission";
+          Group = "multimedia";
+          ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString config.services.transmission.settings.rpc-port}";
+          PrivateNetwork = "yes";
+        };
+      };
+    };
+    # allowing caddy to access transmission in network namespace, a socket is necesarry
+    sockets.proxy-to-transmission = {
+      enable = true;
+      description = "Socket for Proxy to Transmission";
+      listenStreams = [ (toString config.services.transmission.settings.rpc-port) ];
+      wantedBy = [ "sockets.target" ];
+    };
+  };
 }
