9ae26a5842e66b6859226a264cf3ca608a68aa1a
Collin Diekvoss <Collin@Diekvoss.com>
2025-09-17 09:15:06 -0500

    ghostty theme names

--- modules/home/programs/terminals/ghostty.nix
+++ modules/home/programs/terminals/ghostty.nix
@@ -24,7 +24,7 @@
     font-feature = ss08
     font-feature = ss09
     font-feature = liga
-    theme = catppuccin-frappe
+    theme = light:Catppuccin Latte,dark:Catppuccin Frappe
     command = ${lib.getExe pkgs.fish}
     confirm-close-surface = false
     quit-after-last-window-closed = true
