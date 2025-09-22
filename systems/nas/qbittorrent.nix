e008f61edf2c9dfc64eb4fc71ce499e8716bdf57
Collin Diekvoss <Collin@Diekvoss.com>
2025-09-22 17:07:16 -0500

    pf

--- systems/nas/qbittorrent.nix
+++ systems/nas/qbittorrent.nix
@@ -8,7 +8,7 @@
 let
   wireguardInterface = "wg0";
   wireguardInterfaceNamespace = "protonvpn0";
-  wireguardGateway = "10.2.0.1";
+  # wireguardGateway = "10.2.0.1";
   inherit (config.networking) hostName;
 in
 {
@@ -35,7 +35,7 @@ in
           requires = [
             "network-online.target"
             "wireguard-${wireguardInterface}.service"
-            "port-forward-qbittorrent.service"
+            # "port-forward-qbittorrent.service"
             "proxy-to-qbittorrent.service"
           ];
           serviceConfig.NetworkNamespacePath = "/var/run/netns/${wireguardInterfaceNamespace}";
@@ -61,68 +61,67 @@ in
             PrivateNetwork = "yes";
           };
         };
-        port-forward-qbittorrent = {
-          enable = true;
-          bindsTo = [ "wireguard-${wireguardInterface}.service" ];
-          requires = [
-            "network-online.target"
-            "wireguard-${wireguardInterface}.service"
-          ];
-          after = [ "qbittorrent.service" ];
-          description = "Port Forwarding for qBittorrent through ProtonVPN";
-          unitConfig = {
-            JoinsNamespaceOf = "qbittorrent.service";
-          };
-          serviceConfig = {
-            User = "qbittorrent";
-            Group = "multimedia";
-            PrivateNetwork = "yes";
-            ExecStart = pkgs.writeScript "port-forward-qbittorrent.py" ''
-              #!${pkgs.python3}/bin/python3
-              import time
-              import subprocess
+        # port-forward-qbittorrent = {
+        #   enable = true;
+        #   bindsTo = [ "wireguard-${wireguardInterface}.service" ];
+        #   requires = [
+        #     "network-online.target"
+        #     "wireguard-${wireguardInterface}.service"
+        #   ];
+        #   after = [ "qbittorrent.service" ];
+        #   description = "Port Forwarding for qBittorrent through ProtonVPN";
+        #   unitConfig = {
+        #     JoinsNamespaceOf = "qbittorrent.service";
+        #   };
+        #   serviceConfig = {
+        #     User = "qbittorrent";
+        #     Group = "multimedia";
+        #     PrivateNetwork = "yes";
+        #     ExecStart = pkgs.writeScript "port-forward-qbittorrent.py" ''
+        #       #!${pkgs.python3}/bin/python3
+        #       import time
+        #       import subprocess
 
+        #       while True:
+        #           try:
+        #               print(time.strftime("%Y-%m-%d %H:%M:%S"))
+        #               udp_result = subprocess.run(
+        #                   ["${pkgs.libnatpmp}/bin/natpmpc", "-a", "1", "0", "udp", "60", "-g", "${wireguardGateway}"],
+        #                   capture_output=True, text=True
+        #               )
+        #               tcp_result = subprocess.run(
+        #                   ["${pkgs.libnatpmp}/bin/natpmpc", "-a", "1", "0", "tcp", "60", "-g", "${wireguardGateway}"],
+        #                   capture_output=True, text=True
+        #               )
 
-              while True:
-                  try:
-                      print(time.strftime("%Y-%m-%d %H:%M:%S"))
-                      udp_result = subprocess.run(
-                          ["${pkgs.libnatpmp}/bin/natpmpc", "-a", "1", "0", "udp", "60", "-g", "${wireguardGateway}"],
-                          capture_output=True, text=True
-                      )
-                      tcp_result = subprocess.run(
-                          ["${pkgs.libnatpmp}/bin/natpmpc", "-a", "1", "0", "tcp", "60", "-g", "${wireguardGateway}"],
-                          capture_output=True, text=True
-                      )
+        #               if udp_result.returncode == 0 and tcp_result.returncode == 0:
+        #                   # Extract the port from the TCP result
+        #                   for line in tcp_result.stdout.splitlines():
+        #                       if line.startswith("Mapped public port"):
+        #                           port = int(line.split()[3])
+        #                           # TODO: set forwarding port to whatever comes back and ensure the checkbox to use UPnP / NAT-PMP is disabled
+        #                           print(f"TCP port {port} mapped successfully")
+        #                           break
+        #                   # Extract the port from the UDP result
+        #                   for line in udp_result.stdout.splitlines():
+        #                       if line.startswith("Mapped public port"):
+        #                           port = int(line.split()[3])
+        #                           # TODO: set forwarding port to whatever comes back and ensure the checkbox to use UPnP / NAT-PMP is disabled
+        #                           print(f"UDP port {port} mapped successfully")
+        #                           break
+        #               else:
+        #                   print("ERROR with natpmpc command")
+        #                   print("UDP Output:", udp_result.stdout)
+        #                   print("TCP Output:", tcp_result.stdout)
+        #                   break
+        #           except Exception as e:
+        #               print(f"Unexpected error: {e}")
+        #               break
 
-                      if udp_result.returncode == 0 and tcp_result.returncode == 0:
-                          # Extract the port from the TCP result
-                          for line in tcp_result.stdout.splitlines():
-                              if line.startswith("Mapped public port"):
-                                  port = int(line.split()[3])
-                                  # TODO: set forwarding port to whatever comes back and ensure the checkbox to use UPnP / NAT-PMP is disabled
-                                  print(f"TCP port {port} mapped successfully")
-                                  break
-                          # Extract the port from the UDP result
-                          for line in udp_result.stdout.splitlines():
-                              if line.startswith("Mapped public port"):
-                                  port = int(line.split()[3])
-                                  # TODO: set forwarding port to whatever comes back and ensure the checkbox to use UPnP / NAT-PMP is disabled
-                                  print(f"UDP port {port} mapped successfully")
-                                  break
-                      else:
-                          print("ERROR with natpmpc command")
-                          print("UDP Output:", udp_result.stdout)
-                          print("TCP Output:", tcp_result.stdout)
-                          break
-                  except Exception as e:
-                      print(f"Unexpected error: {e}")
-                      break
-
-                  time.sleep(45)
-            '';
-          };
-        };
+        #           time.sleep(45)
+        #     '';
+        #   };
+        # };
       };
       # allowing caddy to access qbittorrent in network namespace, a socket is necesarry
       sockets.proxy-to-qbittorrent = {
