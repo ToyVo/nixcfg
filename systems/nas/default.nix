2372f6c3a9aac0c2e08b87f93831b76d5f88c225
Collin Diekvoss <Collin@Diekvoss.com>
2025-11-11 14:35:03 -0600

    remove ovmf

--- systems/nas/default.nix
+++ systems/nas/default.nix
@@ -240,11 +240,7 @@ in
   virtualisation = {
     libvirtd = {
       enable = true;
-      qemu = {
-        swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-      };
+      qemu.swtpm.enable = true;
     };
     spiceUSBRedirection.enable = true;
   };
