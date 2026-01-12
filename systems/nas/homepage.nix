ff70498a01935f3c94c0429e312a636e3e9d63e2
Collin Diekvoss <Collin@Diekvoss.com>
2026-01-11 23:20:51 -0600

    widget

--- systems/nas/homepage.nix
+++ systems/nas/homepage.nix
@@ -66,53 +66,7 @@ in
       #     ];
       #   }
       # ];
-      services = lib.mapAttrsToList (category: services: { "${category}" = services; }) (
-        lib.foldl' (
-          acc: hostname:
-          let
-            services = homelab.${hostname}.services or { };
-          in
-          lib.foldl' (
-            accInner: service:
-            let
-              displayName = services.${service}.displayName or service;
-              subdomain = services.${service}.subdomain or service;
-              domain = services.${service}.domain or "diekvoss.net";
-              category = services.${service}.category or "TODO";
-              description = services.${service}.description or "TODO";
-              href = if subdomain == "@" then "https://${domain}" else "https://${subdomain}.${domain}";
-              hasWidget = services.${service}.hasWidget or false;
-            in
-            if hasWidget then
-              accInner
-            else
-              accInner
-              // {
-                ${category} = (accInner.${category} or [ ]) ++ [
-                  {
-                    "${displayName}" = {
-                      inherit href description;
-                    };
-                  }
-                ];
-              }
-          ) acc (lib.attrNames services)
-        ) { } (lib.attrNames homelab)
-      );
-      widgets = [
-        {
-          resources = {
-            cpu = true;
-            disk = "/";
-            memory = true;
-          };
-        }
-        {
-          search = {
-            provider = "duckduckgo";
-            target = "_blank";
-          };
-        }
+      services = [
         {
           widget = {
             type = "adguard";
@@ -207,6 +161,53 @@ in
             type = "readarr";
             url = "https://readarr.diekvoss.net";
             key = "{{HOMEPAGE_VAR_READARR_API_KEY}}";
+          };
+        }
+      ] ++ lib.mapAttrsToList (category: services: { "${category}" = services; }) (
+        lib.foldl' (
+          acc: hostname:
+          let
+            services = homelab.${hostname}.services or { };
+          in
+          lib.foldl' (
+            accInner: service:
+            let
+              displayName = services.${service}.displayName or service;
+              subdomain = services.${service}.subdomain or service;
+              domain = services.${service}.domain or "diekvoss.net";
+              category = services.${service}.category or "TODO";
+              description = services.${service}.description or "TODO";
+              href = if subdomain == "@" then "https://${domain}" else "https://${subdomain}.${domain}";
+              hasWidget = services.${service}.hasWidget or false;
+            in
+            if hasWidget then
+              accInner
+            else
+              accInner
+              // {
+                ${category} = (accInner.${category} or [ ]) ++ [
+                  {
+                    "${displayName}" = {
+                      inherit href description;
+                    };
+                  }
+                ];
+              }
+          ) acc (lib.attrNames services)
+        ) { } (lib.attrNames homelab)
+      );
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            disk = "/";
+            memory = true;
+          };
+        }
+        {
+          search = {
+            provider = "duckduckgo";
+            target = "_blank";
           };
         }
       ];
