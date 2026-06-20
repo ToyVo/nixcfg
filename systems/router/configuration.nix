7163cbcb8fbf69ef854f1a53bacbfb397300603d
Collin Diekvoss <Collin@Diekvoss.com>
2026-06-20 11:14:13 -0500

    fix(nixcfg/router): use AUTHENTIK_TOKEN env var for proxy outpost

--- systems/router/configuration.nix
+++ systems/router/configuration.nix
@@ -521,10 +521,11 @@ in
     wants = [ "network-online.target" ];
     environment = {
       AUTHENTIK_HOST = "http://10.200.0.16:9000";
-      AUTHENTIK_TOKEN_FILE = config.sops.secrets."authentik-outpost-token".path;
     };
     serviceConfig = {
-      ExecStart = "${pkgs.authentik-outposts.proxy}/bin/proxy";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'export AUTHENTIK_TOKEN=$(cat ${
+        config.sops.secrets."authentik-outpost-token".path
+      }); exec ${pkgs.authentik-outposts.proxy}/bin/proxy'";
       Restart = "on-failure";
       RestartSec = "30";
     };
