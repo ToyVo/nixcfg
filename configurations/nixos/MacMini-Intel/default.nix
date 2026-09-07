b00d494e4a2f04ca1fe40e3bc120671381256e9b
Collin Diekvoss <Collin@Diekvoss.com>
2026-09-06 21:25:13 -0500

    MacMini-Intel-Nixos fix

--- configurations/nixos/MacMini-Intel/default.nix
+++ configurations/nixos/MacMini-Intel/default.nix
@@ -1,4 +1,32 @@
 {
-  type = "nixos";
+  nixos-stable,
+  nixos-unstable,
+  self,
+  ...
+}@inputs:
+let
+  homelab = import "${self}/homelab.nix";
+  stablePkgs = import nixos-stable {
+    inherit system;
+    config.allowUnfree = true;
+  };
   system = "x86_64-linux";
+  unstablePkgs = import nixos-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in
+nixos-unstable.lib.nixosSystem {
+  inherit system;
+  modules = [ ./configuration.nix ];
+  pkgs = unstablePkgs;
+  specialArgs = {
+    inherit
+      homelab
+      inputs
+      stablePkgs
+      system
+      unstablePkgs
+      ;
+  };
 }
